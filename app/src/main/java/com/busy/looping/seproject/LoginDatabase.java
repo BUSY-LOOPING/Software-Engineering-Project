63939c210b0e2f85426f1427e7a9ba49d39453cb
BUSY-LOOPING <dhruvyadav2905@gmail.com>
2022-03-28 00:58:43 +0530

    Third Commit

--- app/src/main/java/com/busy/looping/seproject/LoginDatabase.java
+++ app/src/main/java/com/busy/looping/seproject/LoginDatabase.java
@@ -1,6 +1,8 @@
 package com.busy.looping.seproject;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -14,12 +16,15 @@ import com.busy.looping.seproject.Util.FileReaderUtil;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginDatabase extends SQLiteOpenHelper {
     private static final String TAG = "MyLoginDatabase";
     private Context context;
     private static final String DATABASE_NAME = "loginDatabase.db";
-    private static final String USER_TABLE_NAME = "user_table";
+    private static final String USER_TABLE_NAME = "users";
     private static WeakReference<LoginDatabase> mInstance;
 
     private String errorMessage = "";
@@ -51,8 +56,10 @@ public class LoginDatabase extends SQLiteOpenHelper {
                 sqLiteDatabase.execSQL(query);
             }
         } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
             errorMessage = sqlException.getMessage();
         } catch (IOException e) {
+            e.printStackTrace();
             errorMessage += "\n" + e.getMessage();
         } catch (Exception e) {
             e.printStackTrace();
@@ -69,6 +76,62 @@ public class LoginDatabase extends SQLiteOpenHelper {
     public void onConfigure(SQLiteDatabase db) {
         super.onConfigure(db);
         db.setForeignKeyConstraintsEnabled(true);
+    }
+
+    public boolean containsEmailPassword(String email, String password) {
+        SQLiteDatabase db = LoginDatabase.getInstance(context).getReadableDatabase();
+        String query = "SELECT * FROM " + USER_TABLE_NAME + " WHERE email_id =? AND hashed_password =? " ;
+        Cursor cursor = db.rawQuery(query, new String[] {email, getHashedPassword(password)});
+        if (cursor.getCount() == 0) {
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        db.close();
+        return true;
+    }
+
+    public static String getHashedPassword(String password) {
+        byte[] encodedHash = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            encodedHash = digest.digest(
+                    password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return bytesToHex(encodedHash);
+    }
+
+    public boolean insertData (String email_id, String password, String role) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email_id", email_id);
+        contentValues.put("hashed_password", getHashedPassword(password));
+        contentValues.put("role", role);
+
+        SQLiteDatabase sqLiteDatabase = LoginDatabase.getInstance(context).getWritableDatabase();
+        long res =sqLiteDatabase.insert(USER_TABLE_NAME, null, contentValues);
+        sqLiteDatabase.close();
+
+        return res != -1;
+    }
+
+    private static String bytesToHex(@Nullable byte[] hash) {
+        if (hash != null) {
+            StringBuilder hexString = new StringBuilder(2 * hash.length);
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } else {
+            return "null";
+        }
     }
 
     public String getErrorMessage() {
--- app/src/main/java/com/busy/looping/seproject/SignUp_SignInActivity.java
+++ app/src/main/java/com/busy/looping/seproject/SignUp_SignInActivity.java
@@ -1,20 +1,34 @@
 package com.busy.looping.seproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.busy.looping.seproject.databinding.ActivitySignUpBinding;
 import com.google.android.material.checkbox.MaterialCheckBox;
 
 public class SignUp_SignInActivity extends AppCompatActivity {
+    public static final int LOGIN_SUCCESSFUL = 100;
+    public static final int LOGIN_UNSUCCESSFUL = 101;
+    public static final int SIGNUP_SUCCESSFUL = 200;
+    public static final int SIGNUP_UNSUCCESSFUL = 201;
     public static final String IS_LOGIN = "LoginActivityIsLogin";
     private ActivitySignUpBinding binding;
     private MaterialCheckBox organizerCheckBox, customerCheckBox;
+    private EditText emailEt, passwordEt;
     private boolean isCustomer = true;
+
+    private String email, password;
 
 
     private boolean isLogin = false;
@@ -53,6 +67,100 @@ public class SignUp_SignInActivity extends AppCompatActivity {
         organizerCheckBox.setOnClickListener(v -> {
             organizerClicked();
         });
+
+        emailEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() != 0) {
+                    email = charSequence.toString();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        passwordEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() != 0) {
+                    password = charSequence.toString();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+//        passwordEt.setOnTouchListener((v, event) -> {
+//            final int DRAWABLE_LEFT = 0;
+//            final int DRAWABLE_TOP = 1;
+//            final int DRAWABLE_RIGHT = 2;
+//            final int DRAWABLE_BOTTOM = 3;
+//
+//            if(event.getAction() == MotionEvent.ACTION_UP) {
+//                if(event.getRawX() >= (passwordEt.getRight() - passwordEt.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+//                    // your action here
+//                    return true;
+//                }
+//            }
+//            return false;
+//        });
+
+        binding.continueBtn.setOnClickListener(v -> {
+            if (isLogin) {
+                if (validateFields()) {
+                    if (signInToDB(this, email, password)) {
+                        Toast.makeText(this, "Sign in successful!", Toast.LENGTH_SHORT).show();
+                        finish();
+                        setResult(LOGIN_SUCCESSFUL);
+                    } else {
+                        Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } else {
+                if (validateFields())
+                    if (signUpToDB(this, email, password, isCustomer)) {
+                        Toast.makeText(this, "Sign up successful!", Toast.LENGTH_SHORT).show();
+                        finish();
+                        setResult(SIGNUP_SUCCESSFUL);
+                    } else {
+                        Toast.makeText(this, "Sign up failed!", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
+    }
+
+    public static boolean signInToDB(Context context, String email, String password) {
+        return LoginDatabase.getInstance(context).containsEmailPassword(email, password);
+    }
+
+    private boolean validateFields() {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEt.setError("Provide a valid email");
+            return false;
+        } else if (password.length() < 6) {
+            passwordEt.setError("Password must be atleast 8 digit long");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean signUpToDB(Context context, String email, String password, boolean isCustomer) {
+
+        return LoginDatabase.getInstance(context).insertData(email, password, isCustomer ? "user" : "admin");
     }
 
     private void customerClicked() {
@@ -68,6 +176,7 @@ public class SignUp_SignInActivity extends AppCompatActivity {
     }
 
     private void init() {
+        email = password = "";
         isLogin = getIntent().getBooleanExtra(IS_LOGIN, false);
         customerCheckBox = binding.customerCheckBox;
         organizerCheckBox = binding.organizerCheckBox;
@@ -85,6 +194,7 @@ public class SignUp_SignInActivity extends AppCompatActivity {
                 binding.signUpBtn.setVisibility(View.GONE);
             }
         }
-
+        emailEt = binding.emailEt;
+        passwordEt = binding.passwordEt;
     }
 }
